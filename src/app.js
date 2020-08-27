b270de6003dcec07bd240db94bd9bb1a2697ffd3
lanjara <herypaslie.dell@gmail.com>
2020-08-27 19:39:17 +0300

    ddd

--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ let stream = require( './ws/stream' );
 let path = require( 'path' );
 let favicon = require( 'serve-favicon' );
 
-const port = 3000;
-
-// app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
+app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
 app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 
 app.get( '/', ( req, res ) => {
@@ -18,6 +16,12 @@ app.get( '/', ( req, res ) => {
 
 io.of( '/stream' ).on( 'connection', stream );
 
-server.listen(port, () => {
-    console.log(`Server running on localhost:${port}`);
+// server.listen(process.env.PORT || port, () => {
+//     console.log(`Server running on localhost:${port}`);
+// });
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8088;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port, server_host);
 });
